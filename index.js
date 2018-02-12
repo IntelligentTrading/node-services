6f3f0f9364532c4b9a9a2903574c965872e208c2
Brbb <franxster@gmail.com>
2018-02-12 23:15:13 +0630

    Hint system service

--- index.js
+++ index.js
@@ -343,7 +343,20 @@ app.route('/api/broadcast').
 
             var slices = Math.ceil(users.length / 20);
 
+            var broadcast_markdown_opts = {
+                parse_mode: "Markdown"
+            }
+
             var replaceables = req.body.replace;
+
+            if (req.body.buttons) {
+                var keyboard = [];
+                var kb_btns = [];
+                req.body.buttons.forEach(btn => kb_btns.push(btn));
+                broadcast_markdown_opts.reply_markup = {
+                    inline_keyboard: [kb_btns]
+                }
+            }
 
             for (current_slice = 0; current_slice < slices; current_slice++) {
                 users.slice(current_slice * 20, 20 * (current_slice + 1) - 1)
@@ -354,7 +367,7 @@ app.route('/api/broadcast').
                             final_message = req.body.text.replace(replaceable.key, user[replaceable.value])
                         })
 
-                        bot.sendMessage(user.telegram_chat_id, final_message, markdown_opts)
+                        bot.sendMessage(user.telegram_chat_id, final_message, broadcast_markdown_opts)
                             .then(console.log(`${user.telegram_chat_id} ok`))
                             .catch(reason => console.log(`${user.telegram_chat_id}:${reason}`));
                     })
