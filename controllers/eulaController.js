b6f918224b2dfc8f9d6e0b9678b35e55768f78be
Brbb <franxster@gmail.com>
2018-04-10 14:46:42 +0700

    [Feature] First history commit

--- controllers/eulaController.js
+++ controllers/eulaController.js
@@ -2,6 +2,7 @@ var marketApi = require('../api/market')
 var UserModel = require('../models/User')
 var bot = require('../util/telegramBot').bot
 var markdown_opts = require('../util/telegramBot').markdown
+var historyCtrl = require('./historyController')
 
 module.exports = {
     render: (request, response) => {
@@ -24,27 +25,26 @@ module.exports = {
                 return user
             }).then((newUser) => {
                 response.render('eula_done')
-                if (newUser)
-                    return 'Thanks for accepting EULA, you can now subscribe with `/token user_token` or keep using the bot with the free plan.'
-                else
-                    return 'You already accepted the EULA, you can now subscribe with `/token user_token` or keep using the bot with the free plan.'
-            }).then(eula_message => {
-                var opts =
-                    {
-                        parse_mode: 'Markdown',
-                        reply_markup: {
-                            inline_keyboard: [[{
-                                "text": "Yes",
-                                "callback_data": JSON.stringify({ cmd: "wizard", n: "hrz" })
-                            },
-                            {
-                                "text": "No",
-                                "callback_data": JSON.stringify({ cmd: "wizard", n: "nevermind" })
-                            }]]
-                        }
-                    }
-                bot.sendMessage(chat_id, eula_message, markdown_opts).then(() => {
-                    bot.sendMessage(chat_id, '💡*Hint*\nWe can walk through few configuration steps or you can do it using a /wizard. Do you wanna do it now?', opts)
+                var eulaDoneMsg = newUser
+                    ? 'Thanks for accepting EULA, you can now check your /settings and subscribe to our plans or keep using the bot with the free plan.'
+                    : 'You already accepted the EULA, you can now check your /settings and upgrade or keep using the bot with the current plan.'
+
+                bot.sendMessage(chat_id, eulaDoneMsg)
+            }).then(() => {
+                historyCtrl.getSignalHistory({
+                    trend: 1,
+                    horizon: 1,
+                    counter_currency: 2,
+                    source: 0
+                }).then(historyEntriesJson => {
+                    bot.sendMessage(chat_id, 'In the meantime, this is a short list of the latest signals sent:')
+
+                    var historyEntries = JSON.parse(historyEntriesJson).results.filter(r => r.signal != 'SMA').slice(0, 3)
+                    historyEntries.forEach(entry => {
+                        var templatedSignal = historyCtrl.applyTemplate(entry)
+                        bot.sendMessage(chat_id, templatedSignal, markdown_opts)
+                    })
+                    //
                 })
             }).catch(reason => {
                 console.log(reason)
