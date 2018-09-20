6e0b4ae21f82d62021aeec7a20cc634f7b249424
Francesco De Lisi <francesco@intelligenttrading.org>
2018-09-20 18:27:42 +0700

    More debugging logs

--- controllers/tradingAlertsController.js
+++ controllers/tradingAlertsController.js
@@ -5,9 +5,17 @@ module.exports = {
         return TradingAlert.create(ta)
     },
     getAll: () => {
-        return TradingAlert.find({}).sort({ signalId: -1 }).limit(100)
+        console.time('Loading top 100 trading alerts')
+        return TradingAlert.find({}).sort({ signalId: -1 }).limit(100).then(result => {
+            console.timeEnd('Loading top 100 trading alerts')
+            return result
+        })
     },
     getLastRejected:() =>{
-        return TradingAlert.$where('this.rejections.length > 5').sort({ signalId: -1 }).limit(1)
+        console.time('Loading rejections for trading alerts')
+        return TradingAlert.$where('this.rejections.length > 5').sort({ signalId: -1 }).limit(1).then(result => {
+            console.timeEnd('Loading rejections for trading alerts')
+            return result
+        })
     }
 }
