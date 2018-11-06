ff3363f99943f7bce72132be113b67618f58dc5f
Francesco De Lisi <francesco@intelligenttrading.org>
2018-11-06 23:28:54 +0700

    Fix promo available number to avoid "nullish" values comparisons

--- models/Promo.js
+++ models/Promo.js
@@ -6,7 +6,7 @@ var promoSchema = new Schema({
     active: { type: Boolean, default: true },
     created: { type: Date, default: Date.now() },
     expiresOn: Date,
-    totalVouchersAvailable: Number,
+    totalVouchersAvailable: { type: Number, default: 100000 },
     totalVouchersRedeemed: { type: Number, default: 0 },
     voucherValue: { type: Number, default: 7 },
     voucherUnit: { type: String, default: 'D' }, // 7 Days
@@ -23,13 +23,13 @@ promoSchema.methods.redeem = function (telegram_chat_id) {
     var redeemedMessage = this.totalVouchersAvailable <= 0 ? this.label + ' is not active or available anymore!' : this.label + ' redeemed correctly!'
     var redeemed = this.totalVouchersAvailable > 0
 
-    if (this.totalVouchersAvailable) {
+    if (this.totalVouchersAvailable > 0 ) {
         this.totalVouchersRedeemed += 1
         this.totalVouchersAvailable -= 1
         console.log(`${redeemedMessage} [${telegram_chat_id}]`)
     }
 
-    this.active = this.totalVouchersAvailable > 0
+    this.active = this.totalVouchersAvailable > 0 // add expiresOn control
     return { redeemed: redeemed, message: redeemedMessage }
 
 }
