1f073ddee5bc2662ae2ef96a04df696c936598db
Francesco De Lisi <francesco@intelligenttrading.org>
2019-01-01 17:48:42 +0700

    Added API to manually register user's payment

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -1,19 +1,29 @@
-var express = require('express')
-var router = express.Router()
-var paymentCtrl = require('../../controllers/paymentController')
-var solve = require('../../util/solver')
+var express = require("express");
+var router = express.Router();
+var paymentCtrl = require("../../controllers/paymentController");
+var solve = require("../../util/solver");
 
+router.post("/verifyTransaction", (req, res) => {
+  solve(paymentCtrl.verifyTransaction(req.body), res);
+});
 
-router.post('/verifyTransaction', (req, res) => {
-    solve(paymentCtrl.verifyTransaction(req.body), res)
-})
+router.post("/verifyEthTransaction", (req, res) => {
+  solve(paymentCtrl.verifyEthTransaction(req.body), res);
+});
 
-router.post('/verifyEthTransaction', (req, res) => {
-    solve(paymentCtrl.verifyEthTransaction(req.body), res)
-})
+router.get("/status/:telegram_chat_id", (req, res) => {
+  solve(paymentCtrl.getUserStatus(req.params.telegram_chat_id), res);
+});
 
-router.get('/status/:telegram_chat_id', (req, res) => {
-    solve(paymentCtrl.getUserStatus(req.params.telegram_chat_id), res)
-})
-
-module.exports = router
+router.post("/register/:telegram_chat_id", (req, res) => {
+  solve(
+    paymentCtrl.registerPaymentByUserId(
+      req.params.telegram_chat_id,
+      req.body.txHash,
+      req.body.amount,
+      req.body.symbol
+    ),
+    res
+  );
+});
+module.exports = router;
